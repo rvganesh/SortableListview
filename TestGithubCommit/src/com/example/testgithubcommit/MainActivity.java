70b00bce8581e516ad3ec586f0072eaedfb669b8
ganesh <ganesh@eventurers.com>
2014-03-31 16:07:35 +0530

    Branch 1 updates.

--- TestGithubCommit/src/com/example/testgithubcommit/MainActivity.java
+++ TestGithubCommit/src/com/example/testgithubcommit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.testgithubcommit;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.d("MainAcitivity", "Branch 1");
 	}
 
 	@Override
