3e51f8ac2b102eee27cd95ea923fa063f855cf50
ganesh <ganesh@eventurers.com>
2014-03-31 16:29:30 +0530

    Branch 2 updated ... now

--- TestGithubCommit/src/com/example/testgithubcommit/MainActivity.java
+++ TestGithubCommit/src/com/example/testgithubcommit/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 		Log.d("MainAcitivity", "Branch 1");
 		Log.d("MainAcitivity", "Branch 2");
 		Log.d("MainAcitivity", "Branch 3");
+		Log.d("MainAcitivity", "Branch 4");
 	}
 
 	@Override
